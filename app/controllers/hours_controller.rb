b9a78f20a96e9c040d0586383e8dda55465ae9bf
Merlyn Albery-Speyer <merlyn@newrelic.com>
2015-03-22 11:39:26 -0700

    Fine tune the display of when is busy.

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -1,16 +1,22 @@
 class HoursController < ApplicationController
   def index
-    races_response = `curl -H "Accept: application/json" -H "X-Query-Key: #{ENV['INSIGHTS_QUERY_KEY']}" "https://insights-api.newrelic.com/v1/accounts/929577/query?nrql=SELECT%20uniqueCount(racer_id),%20average(timestamp)%20from%20RaceDataTest3%20facet%20race_id%20since%201%20weeks%20ago%20limit%201000"`
+    races_response = `curl -H "Accept: application/json" -H "X-Query-Key: #{ENV['INSIGHTS_QUERY_KEY']}" "https://insights-api.newrelic.com/v1/accounts/929577/query?nrql=SELECT%20uniqueCount(racer_id),%20average(timestamp),%20average(duration)%20from%20RaceDataTest3%20facet%20race_id%20since%201%20weeks%20ago%20limit%201000"`
     @races = JSON.parse(races_response)
 
-    @day = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = 0 } }
+    @day = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = 0 } } }
+    @people = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = 1000 } }
 
     @races["facets"].each do |race|
       racers = race["results"][0]["uniqueCount"]
       start_time = Time.at(race["results"][1]["average"]/1000).
         in_time_zone('Pacific Time (US & Canada)') + 7.hours
-      @day[start_time.wday][start_time.hour] += racers
-      puts "#{start_time.wday} #{start_time.hour} -> #{racers} #{@day[start_time.wday][start_time.hour]}"
+      duration = race["results"][2]["average"].to_i
+      offset = start_time
+      duration.times {
+        @day[offset.wday][offset.hour][offset.min] += racers
+        @people[offset.wday][offset.hour] = [@people[offset.wday][offset.hour], racers].min
+        offset += 1.minute
+      }
     end
   end
 end
