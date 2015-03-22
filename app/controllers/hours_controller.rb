10b0ce8550151a8400bf2ef6f265d98baf78d4d0
Merlyn Albery-Speyer <merlyn@newrelic.com>
2015-03-22 10:22:54 -0700

    Add when is busy.

---
+++ app/controllers/hours_controller.rb
@@ -0,0 +1,15 @@
+class HoursController < ApplicationController
+  def index
+    races_response = `curl -H "Accept: application/json" -H "X-Query-Key: #{ENV['INSIGHTS_QUERY_KEY']}" "https://insights-api.newrelic.com/v1/accounts/929577/query?nrql=SELECT%20uniqueCount(racer_id),%20average(timestamp)%20from%20RaceDataTest3%20facet%20race_id%20since%201%20weeks%20ago%20limit%201000"`
+    @races = JSON.parse(races_response)
+
+    @day = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = 0 } }
+
+    @races["facets"].each do |race|
+      racers = race["results"][0]["uniqueCount"]
+      start_time = Time.at(race["results"][1]["average"]/1000) + 7.hours
+      @day[start_time.wday][start_time.hour] += racers
+      puts "#{start_time.wday} #{start_time.hour} -> #{racers} #{@day[start_time.wday][start_time.hour]}"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'races/:id' => 'races#show'
   get 'karts' => 'karts#index'
+  get 'hours' => 'hours#index'
 
   get '/', to: redirect('/races/latest')
 end
